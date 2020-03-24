f95ed9c1d4f6e351e9cf5ebcba5e56019a825613
dangpro54321 <dangnhph09124@fpt.edu.vn>
2020-03-24 17:16:30 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SlideComponent } from './slide/slide.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-
-
 const routes: Routes = [
-  {path:"home", component: HomeComponent},
-  {path:"about", component: AboutComponent},
+
 ];
 
 @NgModule({
