0b1ef0c364ab8ff1156d9a12f6dac45033ced1e4
dangpro54321 <dangnhph09124@fpt.edu.vn>
2020-03-24 20:15:12 +0700

    5 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { ServicesComponent } from "./services/services.component";
 import { ProductListComponent } from "./product-list/product-list.component";
+import { AddProductComponent } from "./add-product/add-product.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { ProductManagerComponent } from "./product-manager/product-manager.component";
+
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
-  {path:"product",component: ProductListComponent},
+  { path: "product", component: ProductListComponent },
   { path: "contact", component: ContactComponent },
-  { path: "services", component: ServicesComponent }
+  { path: "services", component: ServicesComponent },
+  { path: "product/add", component: AddProductComponent },
+  { path: "product/manager", component: ProductManagerComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "404", pathMatch: "full" }
 ];
 
 @NgModule({
