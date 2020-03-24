a10f59ae290e2d4ec0c67a1096add5b4a53ffe33
dangpro54321 <dangnhph09124@fpt.edu.vn>
2020-03-24 20:33:06 +0700

    2 files modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -1,7 +1,7 @@
 import { Product } from './Product';
 export const data: Product[] = [
-    {id : 1, name: 'product 1', desc: 'Mo ta san pham 1'},
-    {id : 2, name: 'product 2', desc: 'Mo ta san pham 2'}, 
-    {id : 3, name: 'product 3', desc: 'Mo ta san pham 3'},
-    {id : 4, name: 'product 4', desc: 'Mo ta san pham 4'},
+    {id : 1, name: 'product 1',price:123, desc: 'Mo ta san pham 1'},
+    {id : 2, name: 'product 2',price:456, desc: 'Mo ta san pham 2'}, 
+    {id : 3, name: 'product 3',price:789, desc: 'Mo ta san pham 3'},
+    {id : 4, name: 'product 4',price:100, desc: 'Mo ta san pham 4'},
 ]
--- src/app/Product.ts
+++ src/app/Product.ts
@@ -1,5 +1,6 @@
 export class Product {
   id: Number;
   name: String;
+  price:Number;
   desc: String;
 }
