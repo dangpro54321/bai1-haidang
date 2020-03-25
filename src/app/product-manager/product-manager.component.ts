398ec08e432cf6f2496ce451a139a04eaf03c44e
dangpro54321 <dangnhph09124@fpt.edu.vn>
2020-03-25 20:59:14 +0700

    1 file modified

--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { Product } from "../Product";
-import { data } from "../MockData";
+import { ProductService } from '../services/product.service'
 @Component({
   selector: "app-product-manager",
   templateUrl: "./product-manager.component.html",
   styleUrls: ["./product-manager.component.css"]
 })
 export class ProductManagerComponent implements OnInit {
-  constructor() {}
+  products: Product[];
+  constructor(
+    private productService: ProductService
+  ) {}
 
-  ngOnInit() {}
-  products = data;
+  ngOnInit():void {
+     this.getProducts();
+  }
+
   remove(pID) {
-    this.products = this.products.filter(p => p.id != pID);
+    this.products = this.productService.removeProduct(pID);
+  }
+  getProducts(){
+    this.productService.getProducts().subscribe(response => this.products = response, error => console.log(error));
   }
 }
